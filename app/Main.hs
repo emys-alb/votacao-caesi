a0eca3b42aa118076681b59780ced2ddfa4a389d
emys-alb <emilly.oliveira@ccc.ufcg.edu.br>
2022-02-21 22:16:17 -0300

    Cria menu

--- app/Main.hs
+++ app/Main.hs
@@ -8,19 +8,36 @@ main :: IO()
 main = do
     conn <- iniciandoDatabase
     putStrLn "Base de dados criada"
-    
-    putStrLn "O primeiro admin tem login e senha: admin"
-    --cadastraAdmin conn "" "" "admin" "admin"
 
+    putStrLn "Menu de ativades: \
+    \\n 1 - Cadastra o primeiro admin\
+    \\n 2 - Cadastra novo administrador"
+
+    inputOpcao <- getLine
+    print (menu (inputOpcao conn))
+
+
+menu :: String -> Connection -> IO ()
+menu opcao conn
+    | opcao == "1" = cadastroPrimeiroAdmin conn
+    | opcao == "2" = cadastroNovoAdmin conn
+
+cadastroPrimeiroAdmin :: Connection -> IO()
+cadastroPrimeiroAdmin conn = do
+    putStrLn "O primeiro admin tem login e senha: admin"
+    cadastraAdmin conn "" "" "admin" "admin"
+
+cadastroNovoAdmin :: Connection -> IO()
+cadastroNovoAdmin conn = do
     putStrLn "Cadastro de novo administrador"
-    
+
     putStrLn "Coloque seu login como administrador"
     loginAdmin <- getLine
     putStrLn "Coloque sua senha como administrador"
-    senhaAdmin <- getLine 
+    senhaAdmin <- getLine
     putStrLn "Coloque o login do administrador"
-    loginNovoAdmin <- getLine 
+    loginNovoAdmin <- getLine
     putStrLn "Coloque a senha do administrador"
-    senhaNovoAdmin <- getLine 
-    
+    senhaNovoAdmin <- getLine
+
     cadastraAdmin conn loginAdmin senhaAdmin loginNovoAdmin senhaNovoAdmin
