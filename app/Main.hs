5e47acde6131de136c548e95c070bca92ef53c50
helen.cavalcanti <helen.cavalcanti@ccc.ufcg.edu.br>
2022-02-22 21:50:49 -0300

    Adicionando método de cadastra estudantes no main

--- app/Main.hs
+++ app/Main.hs
@@ -9,9 +9,18 @@ main = do
     conn <- iniciandoDatabase
     putStrLn "Base de dados criada"
     
-    -- putStrLn "O primeiro admin tem login e senha: admin"
-    -- cadastrarAdmin conn "admin" "admin"
+    putStrLn "O primeiro admin tem login e senha: admin"
+    cadastrarAdmin conn "admin" "admin"
+    cadastroNovosEstudantes conn
 
-    print "Digite o file path"
-    filePath <- getLine
-    cadastraEstudantes conn filePath
+cadastroNovosEstudantes :: Connection -> IO()
+cadastroNovosEstudantes conn = do
+    putStrLn "Cadastro de novos estudantes"
+    putStrLn "Insira seu login como administrador"
+    loginAdmin <- getLine
+    putStrLn "Insira sua senha como administrador"
+    senhaAdmin <- getLine
+    putStrLn "Insira o caminho para o arquivo .csv que deve conter duas colunas (matricula e senha) para cada estudante"
+    caminho <- getLine
+    --adicionar aqui o metodo de verificarAdmin
+    cadastraEstudantes conn caminho
