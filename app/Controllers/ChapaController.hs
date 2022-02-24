39f874a3ccf4e5a0a59c83a0d98004e1ef1f665e
Sofia Rocha <sofia.cavalcanti@virtus.ufcg.edu.br>
2022-02-24 20:51:46 -0300

    .

--- app/Controllers/ChapaController.hs
+++ app/Controllers/ChapaController.hs
@@ -7,7 +7,7 @@ import Database.PostgreSQL.Simple (Connection, execute)
 cadastrarChapa :: Connection -> String -> Int -> Int -> Int-> IO ()
 --verificar se existe um admin (tlvez)
 --permitir insert 
---
+
 cadastrarChapa conn nomeChapa numeroChapa getIdVotacaoChapa(data) numDeVotosChapa = do 
     let i = "INSERT INTO chapa(nomeChapa,\numeroChapa,\getIdVotacao,\numDeVotosChapa) values (?,?,?,?)"
     execute conn i (getAdimin,nomeChapa,numeroChapa,getIdVotacao,numDeVotosChapa)
