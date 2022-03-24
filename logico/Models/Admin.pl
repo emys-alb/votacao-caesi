5d045b1cab778a3027a299a9870c6553dd5b13ab
emys-alb <emilly.oliveira@ccc.ufcg.edu.br>
2022-03-24 16:56:10 -0300

    ajustes cadastro admin

--- logico/Models/Admin.pl
+++ logico/Models/Admin.pl
@@ -7,16 +7,19 @@ verifica_login_senha(Login, Senha):-
     not(eh_vazia(R)).
 
 verifica_admin_cadastrado(Login):-
-    read_csv('admin.csv', Lists),
-    verifica_na_lista(Login, Lists).
+    read_csv('admin.csv', Admins),
+    verifica_na_lista(Login, Admins).
+
 
 get_admin([], _, _, []).
 get_admin([[Login|[Senha|T1]]|T], Login, Senha, [Login|[Senha|T1]]).
 get_admin([_|T], Login, Senha, R) :- 
     get_admin(T, Login, Senha, R).
 
+
 cadastrar_admin(Login, Senha, R) :-
-    open('../Dados/admin.csv', append, File),
+    get_csv_path('admin.csv', Csv_Admin),
+    open(Csv_Admin, append, File),
     writeln(File, (Login, Senha)),
     close(File),
     R = "Admin cadastrado.".
