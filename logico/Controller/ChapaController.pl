7ab36d166e34629001746ff7907f9c5139fde89f
Sofia Rocha <sofia.cavalcanti@virtus.ufcg.edu.br>
2022-03-29 21:08:50 -0300

    faltanto metodos na main

---
+++ logico/Controller/ChapaController.pl
@@ -0,0 +1,4 @@
+:- include('../Models/Chapa.pl').
+
+cadastro_chapa(DataChapa, Result) :-
+    cadastrar_chapa(DataChapa, Result).
---
+++ logico/Models/Chapa.pl
@@ -0,0 +1,23 @@
+:- use_module(library(csv)).
+:- include('../Utils.pl').
+
+
+verifica_chapa_cadastrada(DataChapa) :-
+    read_csv('chapa.csv', Lists),
+    verifica_na_lista(DataChapa, Lists).
+
+cadastrar_chapa(Nome , Numero, "Chapa Cadastrada") :-
+    get_csv_path('chapa.csv', CsvChapa),
+    open(CsvChapa, append, File),
+    gerar_id_chapa(Id), 
+    writeln(File, (Id, Nome, Numero)),
+    close(File).
+
+gerar_id_chapa(1) :-
+    read_csv('chapa.csv', Lists),
+    not(last(Lists, _)).
+
+gerar_id_chapa(Id) :-
+    read_csv('chapa.csv', Lists),
+    last(Lists, [H|_]),
+    Id is H + 1.
